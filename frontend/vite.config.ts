5a54e21fac7b0792fadd50bf539ecba25d8fd888
Admin <zerniereyes@gmail.com>
2025-07-31 20:25:56 +0800

    some changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths({
+      projects: [path.resolve(__dirname, 'tsconfig.app.json')]
+    })
+  ],
 })
